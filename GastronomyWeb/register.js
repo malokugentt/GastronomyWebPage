7984e2714fadf6b7a5dcd18682587ac3278d971f
elirdescent <elshanielird@gmail.com>
2022-12-21 04:01:15 +0100

    changed menu page, register and login forms

--- GastronomyWeb/register.js
+++ GastronomyWeb/register.js
@@ -18,4 +18,39 @@ inputs.forEach((input) => {
 });
 
 
+
+function validate(){
+  // Marrim vlerat e inputit nga forma
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  // Validimi i emrit dhe mbiemrit
+  if (!name.match(/^[A-Z][a-z]+$/)) {
+    alert("Emri duhet të filloj me shkronjë të madhe dhe të përmbajë vetëm tekst!");
+    return false;
+  }
+  if (!surname.match(/^[A-Z][a-z]+$/)) {
+    alert("Mbiemri duhet të filloj me shkronjë të madhe dhe të përmbajë vetëm tekst!");
+    return false;
+  }
+
+  // Validimi i emrit të përdoruesit
+  if (username.length < 8 || username.length > 15) {
+    alert("Emri i përdoruesit duhet të jetë me i gjatë se 8 karaktere dhe me i vogël se 15!");
+    return false;
+  }
+
+  // Validimi i email-it
+  if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+    alert("Email-i duhet të jete standart!");
+  }
+  //Validimi i Password-it
+  if(!password.match(/^[A-Z].*[0-9]{3}$/)){
+      alert("Passwordi duhet te filloj me shkronje te madhe dhe duhet te perfundoj me 3 numra.")
+  }
+
+  };
  
