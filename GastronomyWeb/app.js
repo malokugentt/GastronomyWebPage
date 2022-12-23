f095a8c1f7795f46a0f1e4145d48eac9e699efb1
GentMaloku <gm57330@ubt-uni.net>
2022-12-23 17:59:30 +0100

    update register and login form

--- GastronomyWeb/app.js
+++ GastronomyWeb/app.js
@@ -19,3 +19,23 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+function validate(){
+  var username = document.getElementById("username").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+ 
+  //Validimi i username-it
+  if(!username.match(/^username.length < 8 || username.length > 15/)){
+    alert("Username duhet te jete ma i gjate se 8 e me i shkurt se 15 karaktere");
+  }
+
+  // Validimi i email-it
+  if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+    alert("Email-i duhet të jete standart!");
+  }
+  //Validimi i Password-it
+  if(!password.match(/^[A-Z].*[0-9]{3}$/)){
+      alert("Passwordi duhet te filloj me shkronje te madhe dhe duhet te perfundoj me 3 numra.")
+
+    }
+}
--- GastronomyWeb/register.js
+++ GastronomyWeb/register.js
@@ -23,29 +23,23 @@ inputs.forEach((input) => {
 
 
 function validate(){
-  alert("ffffffffff")
   // Marrim vlerat e inputit nga forma
   var name = document.getElementById("name").value;
   var surname = document.getElementById("surname").value;
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
+  var confirmpassword = document.getElementById("confirmpassword").value;
 
-  // Validimi i emrit dhe mbiemrit
+  // Validimi i emrit
   if (!name.match(/^[A-Z][a-z]+$/)) {
     alert("Emri duhet të filloj me shkronjë të madhe dhe të përmbajë vetëm tekst!");
     return false;
   }
+  //Validimi i mbiemrit
   if (!surname.match(/^[A-Z][a-z]+$/)) {
     alert("Mbiemri duhet të filloj me shkronjë të madhe dhe të përmbajë vetëm tekst!");
     return false;
   }
-
-  // Validimi i emrit të përdoruesit
-  if (username.length < 8 || username.length > 15) {
-    alert("Emri i përdoruesit duhet të jetë me i gjatë se 8 karaktere dhe me i vogël se 15!");
-    return false;
-  }
-
   // Validimi i email-it
   if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
     alert("Email-i duhet të jete standart!");
@@ -54,48 +48,10 @@ function validate(){
   if(!password.match(/^[A-Z].*[0-9]{3}$/)){
       alert("Passwordi duhet te filloj me shkronje te madhe dhe duhet te perfundoj me 3 numra.")
   }
+  if(!confirmpassword.match(/^[A-Z].*[0-9]{3}$/)){
+    alert("Passwordi duhet te filloj me shkronje te madhe dhe duhet te perfundoj me 3 numra.")
+  }
 
   };
  
-  /*
-  document.getElementById("registerbtn").addEventListener("click",function(e){
-    console.log("test")
-    e.preventDefault();
-    
-    var name = document.getElementById("name").value;
-    var surname = document.getElementById("surname").value;
-    //var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
   
-  
-    // Validimi i emrit dhe mbiemrit
-    if (!name.match(/^[A-Z][a-z]+$/)) {
-      alert("Emri duhet të filloj me shkronjë të madhe dhe të përmbajë vetëm tekst!");
-      e.preventDefault();
-     // return false;
-    }
-    if (!surname.match(/^[A-Z][a-z]+$/)) {
-      alert("Mbiemri duhet të filloj me shkronjë të madhe dhe të përmbajë vetëm tekst!");
-      e.preventDefault();
-     // return false;
-    }
-  
-    // Validimi i emrit të përdoruesit
-    if (username.length < 8 || username.length > 15) {
-      alert("Emri i përdoruesit duhet të jetë me i gjatë se 8 karaktere dhe me i vogël se 15!");
-      e.preventDefault();
-     // return false;
-    }
-  
-    // Validimi i email-it
-    if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
-      alert("Email-i duhet të jete standart!");
-      e.preventDefault();
-    }
-    //Validimi i Password-it
-    if(!password.match(/^[A-Z].*[0-9]{3}$/)){
-        alert("Passwordi duhet te filloj me shkronje te madhe dhe duhet te perfundoj me 3 numra.")
-        e.preventDefault();
-    }
-  })*/
